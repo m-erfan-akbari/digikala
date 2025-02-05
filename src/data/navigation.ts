5a42812d23226950a3db671d27a1bdc88583e295
m-erfan-akbari <m.erfan.ak@gmail.com>
2025-02-06 01:36:04 +0330

    Add dummy data.

---
+++ src/data/navigation.ts
@@ -0,0 +1,40 @@
+import {
+  BriefcaseBusiness,
+  CirclePercent,
+  Flame,
+  ShoppingBasket,
+  Smartphone,
+} from "lucide-react";
+
+export const navigationItems = [
+  {
+    id: "wonderful-nav",
+    title: "شگفت‌انگیزها",
+    icon: CirclePercent,
+    href: "https://www.digikala.com/incredible-offers",
+  },
+  {
+    id: "supermarket-nav",
+    title: "سوپرمارکت",
+    icon: ShoppingBasket,
+    href: "https://www.digikala.com/fresh/",
+  },
+  {
+    id: "gold-nav",
+    title: "طلای دیجیتال",
+    icon: BriefcaseBusiness,
+    href: "https://www.digikala.com/gold/welcome/",
+  },
+  {
+    id: "bestseller-nav",
+    title: "پرفروش‌ترین‌ها",
+    icon: Flame,
+    href: "https://www.digikala.com/best-selling/",
+  },
+  {
+    id: "secondhand-nav",
+    title: "خرید کالای کارکرده",
+    icon: Smartphone,
+    href: "https://www.digikala.com/landing/used/?utm_source=DKHEADER&utm_medium=HEADERBTM&utm_campaign=DKHEADER-USED&utm_id=DK-HEADER-USEDLANDING",
+  },
+];
