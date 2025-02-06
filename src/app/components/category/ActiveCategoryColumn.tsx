de5523c49ab5dbb4bbeb559a5ee80f3f32ebb488
m-erfan-akbari <m.erfan.ak@gmail.com>
2025-02-07 02:32:45 +0330

    Add style to ActiveCategoryColumn.

--- src/app/components/category/ActiveCategoryColumn.tsx
+++ src/app/components/category/ActiveCategoryColumn.tsx
@@ -1,6 +1,5 @@
 import { CategoryColumnType } from "@/data/category";
 import { ChevronLeft } from "lucide-react";
-import { Fragment } from "react";
 
 export default function ActiveCategoryColumn({
   column,
@@ -10,20 +9,20 @@ export default function ActiveCategoryColumn({
   return (
     <div className="">
       {column.map((col, index) => (
-        <Fragment key={index}>
-          <h6 className="my-2 flex items-center gap-1 border-r-2 border-r-red-600 px-2 font-bold text-stone-800">
+        <div key={index} className="my-4">
+          <h6 className="my-2 flex items-center gap-1 border-r-2 border-r-red-600 px-2 font-bold text-stone-800 hover:text-red-600">
             {col.title}
             <ChevronLeft className="h-3 w-3" />
           </h6>
           {col.items.map((item, itemIndex) => (
             <span
               key={itemIndex}
-              className="block py-2 font-light text-stone-500"
+              className="block py-2 font-light text-stone-500 hover:text-red-600"
             >
               {item}
             </span>
           ))}
-        </Fragment>
+        </div>
       ))}
     </div>
   );
