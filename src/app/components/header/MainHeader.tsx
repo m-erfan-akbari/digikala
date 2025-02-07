3478c97a96a0b2a062636a61e23b1949bc06f2c2
m-erfan-akbari <m.erfan.ak@gmail.com>
2025-02-07 18:13:55 +0330

    Remove dynamic(lazy) import for HeaderNavigation

--- src/app/components/header/MainHeader.tsx
+++ src/app/components/header/MainHeader.tsx
@@ -2,6 +2,7 @@ import { HorizontalLogo } from "../ui/Logo";
 import dynamic from "next/dynamic";
 import { HeaderButtonsPlaceholder } from "./HeaderButtons";
 import { SearchbarPlaceholder } from "./Searchbar";
+import HeaderNavigation from "./HeaderNavigation";
 
 const DynamicSearchbar = dynamic(() => import("./Searchbar"), {
   // In Next.js v15.1.6, there's a bug when using ssr: false. I work around it by setting ssr: !!false.
@@ -13,11 +14,6 @@ const DynamicHeaderButtons = dynamic(() => import("./HeaderButtons"), {
   // In Next.js v15.1.6, there's a bug when using ssr: false. I work around it by setting ssr: !!false.
   ssr: !!false,
   loading: () => <HeaderButtonsPlaceholder />,
-});
-
-const DynamicHeaderNavigation = dynamic(() => import("./HeaderNavigation"), {
-  // In Next.js v15.1.6, there's a bug when using ssr: false. I work around it by setting ssr: !!false.
-  ssr: !!false,
 });
 
 export default function MainHeader() {
@@ -33,7 +29,7 @@ export default function MainHeader() {
           <DynamicHeaderButtons />
         </div>
 
-        <DynamicHeaderNavigation />
+        <HeaderNavigation />
       </div>
     </header>
   );
