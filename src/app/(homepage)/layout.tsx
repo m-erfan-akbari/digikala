947d33201626af9a0a1565f6434c5db593de1583
m-erfan-akbari <m.erfan.ak@gmail.com>
2025-02-12 01:04:19 +0330

    Add style to layout

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -9,7 +9,7 @@ export default function layout({ children }: PropsType) {
   return (
     <>
       <MainHeader />
-      <main>{children}</main>
+      <main className="flex flex-col gap-y-4">{children}</main>
     </>
   );
 }
