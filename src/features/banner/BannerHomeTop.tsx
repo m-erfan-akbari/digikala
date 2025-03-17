71b1da864576cbcc7c1a0f33aa53b056ad8a5b9d
m-erfan-akbari <m.erfan.ak@gmail.com>
2025-03-17 10:59:36 +0330

    Create BannerHomeTop component.

---
+++ src/features/banner/BannerHomeTop.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Banner } from "./index";
+
+export default function BannerHomeTop({ data }: { data: Banner[] }) {
+  if (!data) return null;
+  return (
+    <section className="container flex flex-wrap justify-between gap-4">
+      {data.map((banner) => (
+        <div
+          key={banner.id}
+          className="relative basis-[calc(50%-8px)] overflow-hidden rounded-xl lg:grow lg:basis-auto"
+          style={{ aspectRatio: "4 / 3" }}
+        >
+          <Image src={banner.webp_image} alt={`تصویر ${banner.title}`} fill />
+        </div>
+      ))}
+    </section>
+  );
+}
---
+++ src/features/banner/index.d.ts
@@ -0,0 +1,25 @@
+export type Banner = {
+  id: number;
+  image: string;
+  webp_image: string;
+  title: string;
+  is_ad: boolean;
+  url: {
+    base: string | null;
+    uri: string;
+  };
+  video: string;
+  description: string;
+  action_title: string;
+  bullet_points: string[];
+  category_id: number | null;
+  show_percentage: number;
+  opening_type: string;
+  ratio_type: string;
+  display_option: string;
+  hex_code: string;
+  video_direct: string | null;
+  is_smart_ads: boolean;
+  mega_menu_id: number | null;
+  zone: string;
+};
